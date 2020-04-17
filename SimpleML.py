6fc22c3268280abf54e9ebd8127e89b343ecae1c
Bradley WIlson <b.t.wilson@sky.com>
2020-04-17 11:37:27 +0100

    Backprop seems to be working
    It seems to be working, but I'm not really sure

--- SimpleML.py
+++ SimpleML.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 class model():
     def __init__(self):
+        #p.random.seed(1)
         #super(model, self).__init__()
         self.layers = []
         #print(input_size, output_size)
@@ -16,8 +17,9 @@ class model():
             layer = self.layers[layerIndex]
             nextLayer = self.layers[layerIndex+1]
             weightShape = np.concatenate((np.asarray(layer.shape), np.asarray(nextLayer.shape)))
-            layer.weights = np.full(weightShape, 0.5)
-            layer.bias = np.full(nextLayer.shape, -1)
+            #layer.weights = np.full(weightShape, 0.5)
+            layer.weights = np.random.random_sample(weightShape)
+            layer.bias = np.random.random_sample(nextLayer.shape)
 
         self.layers[-1].bias = np.full(self.layers[-1].shape, -1)
 
@@ -29,41 +31,79 @@ class model():
             print(layer.name + "  -=-  " + str(layer.shape) + "  -=-  " + str(weightsAmount))
         print("\n\n")
 
-    def train(self, input=None, target=None, epochs=None, loss_function=None):
+    def train(self, input=None, target=None, epochs=None, loss_function=None, learn_rate=0.00001):
         if(loss_function == None): raise ValueError("Invalid loss_function: " + str(loss_function))
         for epoch in range(1, epochs+1):
+            weight_changes = []
+            bias_changes = []
+            lossValue = 0
             for inputs, targets in zip(input, target):
-                prediction = self.step_forward(inputs)
-                lossValue = loss_function(self, targets, prediction)
-                self.step_backward(targets, prediction)
-                print(targets, prediction)
+                prediction = self.predict(inputs)
+                lossValue += loss_function(self, targets, prediction)
+                wc, bc = self.step_backward(targets, prediction, learn_rate)
+                weight_changes.append(wc)
+                bias_changes.append(bc)
+                #self._applyChanges(wc, bc)
+                print(epoch,"/",epochs, prediction, "Loss:", lossValue/len(input))
+            self._applyChanges(np.average(weight_changes, axis=0), np.average(bias_changes, axis=0))
 
-    def step_forward(self, inputs):
+    def predict(self, inputs):
         currentValues = inputs
 
         for layerIndex in range(0, len(self.layers)-1):
             layer = self.layers[layerIndex]
             layer.neurons = currentValues
-            currentValues = layer.activation(self, np.dot(layer.neurons, layer.weights) + layer.bias)
+            
+            if(layer.use_bias == True): currentValues = layer.activation(self, np.dot(layer.neurons, layer.weights) + layer.bias)
+            if(layer.use_bias == False): currentValues = layer.activation(self, np.dot(layer.neurons, layer.weights))
+
 
         lastLayer = self.layers[-1]
-        print(lastLayer.use_bias)
-        if(lastLayer.use_bias == False): return lastLayer.activation(self, currentValues)
-        if(lastLayer.use_bias == True): return lastLayer.activation(self, currentValues+lastLayer.bias)
+        result = None
+        if(lastLayer.use_bias == False): result = lastLayer.activation(self, currentValues)
+        if(lastLayer.use_bias == True): result = lastLayer.activation(self, currentValues+lastLayer.bias)
+        lastLayer.neurons = result
+        return result
 
-    def step_backward(self, actual, prediction):
-        weightChanges = []
-        previousLayerNeurons = actual
-        for layerIndex in range(0, len(self.layers)):
+
+    def step_backward(self, actual, prediction, learn_rate):
+        nextLayersTarget = actual
+        weight_changes = []
+        bias_changes = []
+        for layerIndex in range(0, len(self.layers)-1):
             index = (len(self.layers)-1) - layerIndex
             layer = self.layers[index]
-            print(layer.name)
-            neurons = layer.neurons
-            if(index == len(self.layers)-1):
-                print("at output")
-            print(previousLayerNeurons, neurons)
-            np.dot(previousLayerNeurons, neurons)
-            previousLayerNeurons = neurons
+            nextLayer = self.layers[index-1]
+            #print(layer.name, " --> ", nextLayer.name)
+            accuracy = activations.sigmoid(self, nextLayersTarget - layer.neurons, deriv=True)
+            #print(accuracy)
+            weightAccuracy = nextLayer.weights * accuracy
+            #print(weightAccuracy)
+            #print(nextLayer.weights)
+            #nextLayer.weights += weightAccuracy*learn_rate
+            weight_changes.append(weightAccuracy*learn_rate)
+            #print(nextLayer.weights)
+            nextLayersTarget = np.average(weightAccuracy, axis=1)
+            #print(nextLayersTarget)
+            biasAccuracy = nextLayer.bias * accuracy
+            #print(biasAccuracy)
+            #print(nextLayer.bias)
+            #nextLayer.bias += biasAccuracy*learn_rate
+            bias_changes.append(biasAccuracy*learn_rate)
+            #print(nextLayer.bias)
+
+            #print("\n")
+        return weight_changes, bias_changes
+            
+    def _applyChanges(self, weight_changes, bias_changes):
+        a = 1
+        #print(weight_changes, bias_changes)
+        for layerIndex in range(0, len(self.layers)-1):
+            index = (len(self.layers)-1) - layerIndex
+            layer = self.layers[index]
+            nextLayer = self.layers[index-1]
+            nextLayer.weights += weight_changes[layerIndex]
+            nextLayer.bias += bias_changes[layerIndex]
 
 class _layer():
     def __init__(self, shape=0, name=None, activation=None, use_bias=False):
@@ -85,18 +125,21 @@ class layers:
             _layer.__init__(self, **args)
 
 class activations:
-    def sigmoid(self, x):
+    def sigmoid(self, x, deriv=False):
+        if(deriv == True): return x * (1 - x)
         return 1/(1+np.exp(-x))
-    def relu(self, x):
-       return np.maximum(0,x)
-    def softmax(self, x):
+    def relu(self, x, deriv=False):
+        if(deriv == True):
+            x[x<=0] = 0
+            x[x>0] = 1
+            return x
+        return np.maximum(0,x)
+    def softmax(self, x, deriv=False):
         expo = np.exp(x)
         expo_sum = np.sum(np.exp(x))
         return expo/expo_sum
-    def none(self, x):
+    def none(self, x, deriv=False):
         return x
-    def sigmoid_dev(self, x):
-        return x * (1 - x)
 
 class loss:
     def mean_squared_error(self, actual, predicted):
--- test.py
+++ test.py
@@ -1,18 +1,23 @@
 import numpy as np
 import SimpleML as sml
 
-inputData = np.array([[2]])
-outputData = np.array([[1]])
+inputData = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]])
+outputData = np.array([[1, 0, 1, 0, 1, 0, 1], [0, 1, 0, 1, 0, 1, 0]])
+
+useBias = False
 
 model = sml.model()
 
-model.add_layer(sml.layers.fully_connected(shape=inputData.shape[1:], name="Input"))
-model.add_layer(sml.layers.fully_connected(shape=[2]))
-model.add_layer(sml.layers.fully_connected(shape=[2]))
-model.add_layer(sml.layers.fully_connected(shape=outputData.shape[1:], name="Output"))
+model.add_layer(sml.layers.fully_connected(shape=inputData.shape[1:], name="Input", activation=sml.activations.relu, use_bias=useBias))
+model.add_layer(sml.layers.fully_connected(shape=[24], activation=sml.activations.relu, use_bias=useBias))
+#model.add_layer(sml.layers.fully_connected(shape=[5], activation=sml.activations.softmax, use_bias=useBias))
+model.add_layer(sml.layers.fully_connected(shape=outputData.shape[1:], name="Output", activation=sml.activations.sigmoid, use_bias=True))
 
 model.compile()
 
 model.display()
 
-model.train(input=inputData, target=outputData, epochs=1, loss_function=sml.loss.mean_squared_error)
+model.train(input=inputData, target=outputData, epochs=100000, loss_function=sml.loss.mean_squared_error, learn_rate=0.0001)
+
+for inp, out in zip(inputData, outputData):
+    print(out, model.predict(inp))
