c021c81f3b95444b6d1daebda3915a2e3705dd3a
itzbradmc <30415619+itzbradmc@users.noreply.github.com>
2020-04-21 02:41:30 +0100

    Pip Package
    Use 'from SimpleML import SimpleML'. Will be fixed later
    
    Added a progress bar for each epoch

--- SimpleML/SimpleML.py
+++ SimpleML/SimpleML.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import sys
 class model():
     def __init__(self):
         np.random.seed(1)
@@ -18,10 +19,10 @@ class model():
             nextLayer = self.layers[layerIndex+1]
             weightShape = np.concatenate((np.asarray(layer.shape), np.asarray(nextLayer.shape)))
             #layer.weights = np.full(weightShape, 0.5)
-            layer.weights = np.random.random_sample(weightShape)
-            layer.bias = np.random.random_sample(nextLayer.shape)
+            layer.weights = np.full(weightShape, 1,dtype='float32')#np.random.random_sample(weightShape)
+            layer.bias = np.full(nextLayer.shape, -1,dtype='float32')#np.random.random_sample(nextLayer.shape)
 
-        self.layers[-1].bias = np.full(self.layers[-1].shape, -1)
+        self.layers[-1].bias = np.full(self.layers[-1].shape, -1,dtype='float32')
 
     def display(self):
         print("\n\n")
@@ -40,18 +41,23 @@ class model():
             bias_changes = []
             lossValue = 0
             accuracyValue = 0
+            stepCount = 0
             for inputs, targets in zip(input, target):
                 prediction = self.predict(inputs)
                 lossValue += loss_function(self, targets, prediction)
                 accuracyValue +=  1 - np.sum(targets-prediction)
-                print(targets, prediction)
+                #print(targets, prediction)
                 wc, bc = self.step_backward(targets, prediction, learn_rate)
                 weight_changes.append(wc)
                 bias_changes.append(bc)
                 #self._applyChanges(wc, bc)
+                progress = stepCount/len(input)
+                #print(progress)
+                self.update_progress(progress, extra=(lossValue/stepCount))
+                stepCount += 1
             lossHistory.append(lossValue/len(input))
             accuracyHistory.append(accuracyValue/len(input))
-            print(epoch,"/",epochs, "Loss:", lossValue/len(input), "Accracy:", accuracyValue/len(input))
+            print(" - ", epoch,"/",epochs, "Loss:", lossValue/len(input), "Accracy:", accuracyValue/len(input))
             self._applyChanges(np.average(weight_changes, axis=0), np.average(bias_changes, axis=0))
         return lossHistory, accuracyHistory
 
@@ -84,7 +90,7 @@ class model():
             nextLayer = self.layers[index-1]
             #print(layer.name, " --> ", nextLayer.name)
             accuracy = layer.activation(self,nextLayersTarget - layer.neurons, deriv=True)
-            #print(accuracy)
+            #print(accuracy.shape, nextLayer.weights.shape)
             weightAccuracy = nextLayer.weights * accuracy
             #print(weightAccuracy)
             #print(nextLayer.weights)
@@ -112,6 +118,25 @@ class model():
             nextLayer = self.layers[index-1]
             nextLayer.weights += weight_changes[layerIndex]
             nextLayer.bias += bias_changes[layerIndex]
+
+    def update_progress(self, progress, extra=0):
+        barLength = 10 # Modify this to change the length of the progress bar
+        status = ""
+        if isinstance(progress, int):
+            progress = float(progress)
+        if not isinstance(progress, float):
+            progress = 0
+            status = "error: progress var must be float\r\n"
+        if progress < 0:
+            progress = 0
+            status = "Halt...\r\n"
+        if progress >= 1:
+            progress = 1
+            status = "Done...\r\n"
+        block = int(round(barLength*progress))
+        text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status) + " - " + str(extra)
+        sys.stdout.write(text)
+        sys.stdout.flush()
 
 class _layer():
     def __init__(self, shape=0, name=None, activation=None, use_bias=False):
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SimpleML',
+    url='https://github.com/itzbradmc/SimpleML',
+    author='Bradley Wilson',
+    author_email='None atm',
+    packages=['SimpleML'],
+    install_requires=['numpy'],
+    version='0.0.1',
+    license='MIT',
+    description='https://github.com/itzbradmc/SimpleML',
+)
